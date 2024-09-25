cc55f6ed44ecec6ed9b058090325a6ec88e7aebd
Hugo Mateus Ribeiro Cardoso Veloso <hugocardosoveloso@gmail.com>
2024-09-25 01:56:34 -0300

    adds map to search

---
+++ public_html/search/search.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-22.81743, -47.06189], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
